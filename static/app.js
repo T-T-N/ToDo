958b724c5f946ebcf0272f861fa231fa6e8e2439
T-T-N <nhamott@gmail.com>
2025-08-05 20:28:24 +0100

    Move CSS and JS to static files for better formatting

---
+++ static/app.js
@@ -0,0 +1,8 @@
+// Paste all JavaScript from your app.py's index.html <script> tag here
+// Example:
+document.addEventListener('DOMContentLoaded', function() {
+    loadTasks();
+    renderCalendar();
+    setupDragAndDrop();
+});
+// ...rest of your JS...
